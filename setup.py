26884b2ea2a04f218ebaad74ece92d6b7e1a0f1d
Hearot <hearotdev@gmail.com>
2017-10-14 16:25:03 +0200

    Change main informations

--- setup.py
+++ setup.py
@@ -26,12 +26,12 @@ setup(
     name='argoscuolanext',
     packages=['argoscuolanext'],
     package_dir={'': 'src'},
-    version='0.5',
+    version='0.5.1',
     description='ArgoScuolaNext Python API',
     author='Gabriel Hearot',
     author_email='gabriel@hearot.it',
     url='https://github.com/hearot/ArgoScuolaNext-Python',
-    download_url='https://github.com/hearot/ArgoScuolaNext-Python/archive/v0.5.tar.gz',
+    download_url='https://github.com/hearot/ArgoScuolaNext-Python/archive/v0.5.1.tar.gz',
     keywords=['argoscuolanext', 'python', 'school' 'api', 'rest'],
     classifiers=[]
 )
